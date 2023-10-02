83817e9192c08c28553e2ea0ad4f12223dc7ce1d
Artur Moreno <113005356+arthmdc0@users.noreply.github.com>
2023-10-02 00:16:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use("/api", apiRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-   console.log(`Servidor em execução na porta ${PORT}`);
+   console.log(`Servidor em execução no endereço http://localhost:3000/inicio.html`);
 });
